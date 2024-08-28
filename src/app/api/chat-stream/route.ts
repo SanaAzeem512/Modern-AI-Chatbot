cd0d867eb1143d457b2afb157cc398774f36b86e
SanaAzeem512 <sanaazeem416@gmail.com>
2024-08-29 01:17:48 +0530

    initial commit

---
+++ src/app/api/chat-stream/route.ts
@@ -0,0 +1,13 @@
+import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
+import { NextRequest } from "next/server";
+import { ragChat } from "../../lib/rag-chat";
+
+export const POST = async (req: NextRequest) => {
+  const { messages, sessionId } = await req.json();
+
+  const lastMessage = messages[messages.length - 1].content;
+
+  const response = await ragChat.chat(lastMessage, { streaming: true, sessionId });
+
+  return aiUseChatAdapter(response);
+};
